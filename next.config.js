910e2239a951c3f8674bb6d62f9182a4e8d74f95
= <tech@functionup.org>
2024-04-17 11:30:38 +0530

    feat : next config

--- next.config.js
+++ next.config.js
@@ -3,17 +3,5 @@ module.exports = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      net: false,
-      dns: false,
-      child_process: false,
-      tls: false,
-    };
-
-    return config;
-  },
+  }
 }
